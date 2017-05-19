f621890fdc8a955908e7d8f377d1a67b6b36301e
xiaogang0-0 <361204134@qq.com>
2017-05-19 19:17:24 +0800

    1

--- wx-index/js/script.js
+++ wx-index/js/script.js
@@ -2,17 +2,16 @@
 * @Author: user
 * @Date:   2017-05-12 10:19:18
 * @Last Modified by:   user
-* @Last Modified time: 2017-05-17 10:11:25
+* @Last Modified time: 2017-05-19 10:16:34
 */
 
 (function() {
   "use strict";
-
   
  //rem 适配:
     var html = document.querySelector("html");          //获取html
     var width = html.getBoundingClientRect().width;   //获取html的宽度
-    html.style.fontSize = width/20+ "px";                   //(设置html的宽度是屏幕宽度的16之1得出来的结果不能小于12)  1000设计图
+    html.style.fontSize = width/20+ "px";                   //1000设计图  1rem = 50px;
 
 
 
