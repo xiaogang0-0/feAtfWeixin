44442605842d75cf886f72d520f2690b810c5249
xiaogang0-0 <361204134@qq.com>
2017-06-02 19:38:19 +0800

    1

--- wx-index/js/script.js
+++ wx-index/js/script.js
@@ -2,114 +2,97 @@
 * @Author: user
 * @Date:   2017-05-12 10:19:18
 * @Last Modified by:   user
-* @Last Modified time: 2017-06-01 20:09:26
+* @Last Modified time: 2017-06-02 13:49:02
 */
 
 (function() {
   "use strict";
-
-    window.addEventListener('orientationchange', function(event){
-          if ( window.orientation == 180 || window.orientation==0 ) {//竖屏事件处理
-               location.reload();//刷新页面
-               alert("竖屏");    
-          }
-          if( window.orientation == 90 || window.orientation == -90 ) {//横屏事件处理
-               location.reload();//刷新页面
-               alert("横屏");    
-          }
+  
+   $(window).resize(function() {
+          location.reload();//刷新页面
+         
       });
- //rem 适配:
+
+    // window.addEventListener('orientationchange', function(event){
+    //     if ( window.orientation == 180 || window.orientation==0 ) {//竖屏事件处理
+    //          location.reload();//刷新页面
+    //     }
+    //     if( window.orientation == 90 || window.orientation == -90 ) {//横屏事件处理
+    //          location.reload();//刷新页面   
+    //     }
+    // });
+
+//rem 适配:
     var html = document.querySelector("html");          //获取html
     var width = html.getBoundingClientRect().width;   //获取html的宽度
     html.style.fontSize = width/20+ "px";                   //1000设计图  1rem = 50px;
 
 
-  // alert(window.devicePixelRatio) ;//自动显示 缩放倍数
 
-
-// // 监听屏幕方向变化，通过MediaQueryList对象的addListener方法
-// var mql = window.matchMedia('(orientation: portrait)');
-// console.log(mql);
-// function handleOrientationChange(mql) {
-// if(mql.matches) {
-// console.log('portrait'); // 打印竖屏
-// alert(0)
-// }else {
-// console.log('landscape'); // 打印横屏
-// // 打印日志
-// handleOrientationChange(mql);
-// // 监听屏幕模式的变化
-// mql.addListener(handleOrientationChange);
-
-// }
-// }
 
 
 
 
 //热门课程的 搜索选项操作
-(function(){
+// (function(){
       
-     var $parent = $('.rmkc-head');             //整个tab的腹肌
-     var $btns = $parent.find('.js-bnt');     //二级切换的父级按钮
-     var $lists =$parent.find('.js-list ul');   // 下面对应的展开ul
-     var $off = $parent .find('i');             // 图标标签
-     var $search = $off.eq(3);               //点击搜索按钮
-     var $parentList = $parent.find('section');
-     var arr = [0,0,0];
-      $btns.click(function(){//一级切换
-  
-          var index = $(this).index();
-          var num = $(document).height()-$('.rmkc-head').height();
+//      var $parent = $('.rmkc-head');             //整个tab的腹肌
+//      var $btns = $parent.find('.js-bnt');     //二级切换的父级按钮
+//      var $lists =$parent.find('.js-list ul');   // 下面对应的展开ul
+//      var $off = $parent .find('i');             // 图标标签
+//      var $search = $off.eq(3);               //点击搜索按钮
+//      var $parentList = $parent.find('section');
+//      var arr = [0,0,0];
+    
+//     $btns.click(function(){//一级切换
+//         var index = $(this).index();
+//         var num = $(document).height()-$('.rmkc-head').height();
 
-          $parentList.css('height',num);//设置 显示遮罩高度
-          $btns.removeClass('active');
-          $(this).addClass('active');            //切换选中样式
-          $('.js-list').removeClass("none");    //二级整体显示
-          $lists.addClass('none');                //清洗二级全隐藏
-          $off.removeClass('icon-shouqi').addClass('icon-zhankai');  //清洗标签图样式还原
-          $lists.eq(index).removeClass('none') ;  //当前的显示
-          $off.eq(index).removeClass('icon-zhankai').addClass("icon-shouqi"); //当前图标更换
-    })
+//         $parentList.css('height',num);//设置 显示遮罩高度
+//         $btns.removeClass('active');
+//         $(this).addClass('active');            //切换选中样式
+//         $('.js-list').removeClass("none");    //二级整体显示
+//         $lists.addClass('none');                //清洗二级全隐藏
+//         $off.removeClass('icon-shouqi').addClass('icon-zhankai');  //清洗标签图样式还原
+//         $lists.eq(index).removeClass('none') ;  //当前的显示
+//         $off.eq(index).removeClass('icon-zhankai').addClass("icon-shouqi"); //当前图标更换
+//     })
 
 
-      $lists.find('li').click(function(){//二级下拉的点击 
-            var index1 = $(this).parent('ul').index();  
-            $btns.eq(index1).find('em').html(this.innerHTML);
-            arr[index1] = this.innerHTML
-             // console.log(index1,$(this).index(),arr)
+//     $lists.find('li').click(function(){//二级下拉的点击 
+//           var index1 = $(this).parent('ul').index();  
+//           $btns.eq(index1).find('em').html(this.innerHTML);
+//           arr[index1] = this.innerHTML
+//            // console.log(index1,$(this).index(),arr)
                
-     })
+//      })
 
-    $parentList.on('click',function(){//点击空白区域隐藏
-            $(this).addClass('none')
-            $off.removeClass('icon-shouqi').addClass('icon-zhankai');  //清洗标签图样式还原
-            $btns.removeClass('active');   //清空当一级的样式         
-      })
+//     $parentList.on('click',function(){//点击空白区域隐藏
+//             $(this).addClass('none')
+//             $off.removeClass('icon-shouqi').addClass('icon-zhankai');  //清洗标签图样式还原
+//             $btns.removeClass('active');   //清空当一级的样式         
+//       })
 
     
-    $search.on('click',function(){//点击搜索时间的接口
-        console.log(arr)
-    })
-})()
+//     $search.on('click',function(){//点击搜索时间的接口
+//         console.log(arr)
+//     })
+// })()
 
 //师资简介的 搜索选项操作
 
-
-shizijianjie()
-
+// shizijianjie()
 function shizijianjie(){
      var $parent = $('.szjj-head');             //整个tab的腹肌
      var $btns = $parent.find('.js-bnt');     //二级切换的父级按钮
      var $lists =$parent.find('.js-list ul');   // 下面对应的展开ul
      var $off = $parent .find('i');             // 图标标签
      var $parentList = $parent.find('.js-list');//二级切换外框
-     var onoff = true;
-
+     
     $btns.click(function(){//一级切换
           var index = $(this).index();
           var num = $(document).height()-$('.szjj-head').height();
-  alert(1)
+         
           if(index ==2){
               index =1
           }
@@ -139,11 +122,6 @@ function shizijianjie(){
      })
 
 }
-
-
-
-
-
 
 
 
